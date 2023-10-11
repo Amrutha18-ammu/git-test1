45c40dbd1b3260cd96c303230758d9d1c78fc385
Amrutha Yarlagadda <amrutha1827@gmail.com>
2023-10-11 11:19:49 -0400

    I have added mongodb to fetch data

--- server.js
+++ server.js
@@ -1,17 +1,37 @@
  const express = require('express');
  const cors = require('cors');
  const app = express();
- const port = 3000;
-
- app.use(cors()); 
+ const mongoose = require('mongoose');
+ const Schema = mongoose.Schema;
  
- const budget = require('./budget-data.json')
+ const port = 3000;
+ app.use(express.json());
+ app.use(cors()); 
 
- app.get('/budget', (req,res) =>{
-    res.json(budget);
- });  
+ const budgetSchema = new Schema({
+  title: { type: String,required: true},
+  relatedValue: { type: Number, required: true },
+  color: { type: String, required: true,minlength: 6, match: /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/},
+ })
+ const conn = mongoose.createConnection('mongodb://127.0.0.1:27017/nbad');
+ const Budget = conn.model('Budget', budgetSchema); 
 
+app.get('/budgets', async (req,res) => {
+   let budgetList =  await Budget.find({});
+   res.json( { myMonthlyBudget: budgetList})
+})
 
+app.post('/budget', async (req,res) => {
+   let budgetReq = req.body
+   console.log(req.body)
+   try {
+      let response = await Budget.create({ title: budgetReq.title, relatedValue: budgetReq.relatedValue, color: budgetReq.color });
+      res.status(200).send(response.title +  " created successfully")
+   } catch (e) {
+      res.status(400).send(e.message)
+   }
+  
+})
  
  app.listen(port, () =>{
     console.log(` API Served at http://localhost:${port}`)
